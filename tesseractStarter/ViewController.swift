4bd2166592717ef47a1202f42f06de1a3049c5ed
thecodingone <amanda.southworth@myds.us>
2017-08-06 21:10:24 -0700

    added AVCaptureSession()

--- tesseractStarter/ViewController.swift
+++ tesseractStarter/ViewController.swift
@@ -7,16 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let captureDev = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            
+            let input = try AVCaptureDeviceInput(device: captureDev)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            
+        } catch {
+            
+            print(error)
+            
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        previewLayer?.frame = captureView.layer.bounds
+        captureView.layer.addSublayer(previewLayer!)
+        
+        captureSession?.startRunning()
         
     }
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captureView: UIView!
+    
+    var captureSession: AVCaptureSession?
+    var previewLayer: AVCaptureVideoPreviewLayer?
     
     @IBOutlet weak var startScan: UIButton!
     @IBAction func startAction(_ sender: Any) {
