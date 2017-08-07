be136f51dc7bf6ff3d5284b18583ad405f2c9195
thecodingone <amanda.southworth@myds.us>
2017-08-06 20:39:18 -0700

    akhRH

---
+++ Pods/Target Support Files/Pods-tesseractStarter/Pods-tesseractStarter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tesseractStarter : NSObject
+@end
+@implementation PodsDummy_Pods_tesseractStarter
+@end
---
+++ tesseractStarter/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  tesseractStarter
+//
+//  Created by Amanda Southworth on 8/6/17.
+//  Copyright © 2017 Amanda Southworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var startScan: UIButton!
+    @IBAction func startAction(_ sender: Any) {
+        
+        
+    }
+    
+    @IBOutlet weak var stopScan: UIButton!
+    @IBAction func stopAction(_ sender: Any) {
+        
+        
+    }
+    
+    @IBOutlet weak var textView: UITextView!
+}
+
