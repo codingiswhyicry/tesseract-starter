62b150fac0992c0778a3c7f233af485db2349d7a
thecodingone <amanda.southworth@myds.us>
2017-08-15 13:13:27 -0700

    changed app layout and added tesseract

--- tesseractStarter/ViewController.swift
+++ tesseractStarter/ViewController.swift
@@ -7,38 +7,21 @@
 //
 
 import UIKit
-import AVFoundation
-import Vision
 import TesseractOCR
 
 class ViewController: UIViewController, G8TesseractDelegate {
+    
+    func recognizeText(image: UIImage) {
+        
+        
+        
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tesseract?.delegate = self
-        
-        
-        let captureDev = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-        
-        do {
-            
-            let input = try AVCaptureDeviceInput(device: captureDev)
-            captureSession = AVCaptureSession()
-            captureSession?.addInput(input)
-            
-        } catch {
-            
-            print(error)
-            
-        }
-        
-        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
-        previewLayer?.frame = captureView.layer.bounds
-        captureView.layer.addSublayer(previewLayer!)
-        
-        captureSession?.startRunning()
         
     }
     
@@ -49,25 +32,59 @@ class ViewController: UIViewController, G8TesseractDelegate {
     
     var tesseract = G8Tesseract(language: "eng")
     
-    @IBOutlet weak var captureView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
     
-    var captureSession: AVCaptureSession?
-    var previewLayer: AVCaptureVideoPreviewLayer?
-    var imageOutput: AVCapturePhotoOutput?
+    @IBOutlet weak var textLabel: UILabel!
     
-    @IBOutlet weak var startScan: UIButton!
-    @IBAction func startAction(_ sender: Any) {
+    @IBOutlet weak var image1: UIButton!
+    @IBAction func imageOneAction(_ sender: Any) {
         
-        captureSession?.startRunning()
     }
     
-    @IBOutlet weak var stopScan: UIButton!
-    @IBAction func stopAction(_ sender: Any) {
+    @IBOutlet weak var image2: UIButton!
+    @IBAction func imageTwoAction(_ sender: Any) {
         
-        captureSession?.stopRunning()
     }
     
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var image3: UIButton!
+    @IBAction func imageThreeAction(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var image4: UIButton!
+    @IBAction func imageFourAction(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var image5: UIButton!
+    @IBAction func imageFiveAction(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var image6: UIButton!
+    @IBAction func imageSixAction(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var image7: UIButton!
+    @IBAction func imageSevenAction(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var image8: UIButton!
+    @IBAction func imageEightAction(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var image9: UIButton!
+    @IBAction func imageNineAction(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var image10: UIButton!
+    @IBAction func imageTenAction(_ sender: Any) {
+        
+    }
+    
     
 }
-
